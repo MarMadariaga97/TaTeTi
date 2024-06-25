6d36c474353ca921e28d0dd36d8beac571249cda
marmadariaga <105765107+MarMadariaga97@users.noreply.github.com>
2024-06-25 14:28:46 -0300

    Add files via upload

---
+++ src/juego/Juego.java
@@ -0,0 +1,89 @@
+package juego;
+
+public class Juego {
+	private String[][] tabla;
+	private static int turno = 0;
+
+	public Juego() {
+		this.tabla = new String[3][3];
+	}
+
+	public String[][] getTabla() {
+		return this.tabla;
+	}
+	
+	public void setTabla(int i, int j, String s) {
+		this.tabla[i][j]=s;
+	}
+
+	public String getSimbolo() {
+		if (turno % 2 == 0) {
+			turno = turno + 1;
+			return "X";
+		}
+
+		else {
+			turno = turno + 1;
+			return "O";
+		}
+
+	}
+
+	private boolean alineacionHorizontal(String s) {
+		if (tabla[0][0].equals(s) && tabla[0][1].equals(s) && tabla[0][2].equals(s)) {
+			return true;
+		} else if (tabla[1][0].equals(s) && tabla[1][1].equals(s) && tabla[1][2].equals(s)) {
+			return true;
+		} else if (tabla[2][0].equals(s) && tabla[2][1].equals(s) && tabla[2][2].equals(s)) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	private boolean alineacionVertical(String s) {
+		if (tabla[0][0].equals(s) && tabla[1][0].equals(s) && tabla[2][0].equals(s)) {
+			return true;
+		} else if (tabla[0][1].equals(s) && tabla[1][1].equals(s) && tabla[2][1].equals(s)) {
+			return true;
+		} else if (tabla[0][2].equals(s) && tabla[1][2].equals(s) && tabla[2][2].equals(s)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public boolean alineacionHorizontalIzqDer(String s) {
+		if (tabla[0][0].equals(s)) {
+			if (tabla[1][1].equals(s) && tabla[2][2].equals(s)) {
+				return true;
+			}
+			return false;
+		}
+
+		else
+			return false;
+	}
+
+	private boolean alineacionHorizontalDerIzq(String s) {
+		if (tabla[2][2].equals(s)) {
+			if (tabla[1][1].equals(s) && tabla[0][0].equals(s)) {
+				return true;
+			}
+			return false;
+		}
+
+		else
+			return false;
+	}
+
+	public boolean hayGanador(String s) {
+		if (alineacionHorizontal(s) || alineacionVertical(s) || alineacionHorizontalIzqDer(s)
+				|| alineacionHorizontalDerIzq(s)) {
+			return true;
+		}
+		return false;
+	}
+
+}
